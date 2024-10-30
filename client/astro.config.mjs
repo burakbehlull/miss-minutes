2a8b668236352c2b60b60075099c623c631a363d
burakbehlull <burakyabgu@gmail.com>
2024-10-30 03:38:55 +0300

    chore: Set up sass

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,5 +3,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	server: { port: 8000 }
-});
+	server: { port: 8000 },
+	vite: {
+    resolve: {
+      alias: {
+        '@sass': '/src/sass',
+		},
+	  },
+	}
+})
