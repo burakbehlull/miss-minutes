21821f626dc67e5177dcb7ed9429f9a548ac6a52
burakbehlull <burakyabgu@gmail.com>
2024-10-31 12:44:26 +0300

    feat: Add react library and create hero component

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-	server: { port: 8000 },
-	vite: {
-    resolve: {
-      alias: {
-        '@sass': '/src/sass',
-		},
+  server: { port: 8000 },
+
+  vite: {
+  resolve: {
+    alias: {
+      '@sass': '/src/sass',
+      },
 	  },
-	}
+	},
+
+  integrations: [react()]
 })
